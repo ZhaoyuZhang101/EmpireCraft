7b9f29810bd6b8fa10201c758db27701fc8bdd3a
Zhaoyu Zhang <1304777288@qq.com>
2025-09-02 17:29:37 +1000

    清空

--- Scripts/AI/ActorAI/GameAIActorBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using ai.behaviours;
-
-namespace EmpireCraft.Scripts.AI.ActorAI;
-
-public abstract class GameAIActorBase: BehaviourActionActor
-{
-    public abstract Type OriginalBeh { get; }
-}
--- Scripts/AI/KingdomAI/GameAIKingdomBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using ai.behaviours;
-
-namespace EmpireCraft.Scripts.AI.KingdomAI;
-
-public abstract class GameAIKingdomBase : BehaviourActionKingdom
-{
-    public abstract Type OriginalBeh { get; }
-}
