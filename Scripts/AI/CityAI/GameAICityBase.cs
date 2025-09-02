7e58136bfe67c4c2e713f9bb446aaa0d70d37303
Zhaoyu Zhang <1304777288@qq.com>
2025-09-02 17:15:39 +1000

    删除之前的逻辑

--- Scripts/AI/CityAI/GameAICityBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using ai.behaviours;
-
-namespace EmpireCraft.Scripts.AI.CityAI;
-
-public abstract class GameAICityBase : BehaviourActionCity
-{
-    public abstract Type OriginalBeh { get; }
-}
