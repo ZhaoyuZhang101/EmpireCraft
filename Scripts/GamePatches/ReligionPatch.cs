f9f198f478bc6b1f0b93febc368665b87577fdc6
Zhaoyu Zhang <1304777288@qq.com>
2025-12-08 02:59:13 +1100

    Changes

--- Scripts/GamePatches/ReligionPatch.cs
+++ Scripts/GamePatches/ReligionPatch.cs
@@ -20,7 +20,7 @@ public class ReligionPatch : GamePatch
         ModPath = declare.FolderPath + "/Locales/";
         new Harmony(nameof(set_religion_name)).Patch(AccessTools.Method(typeof(Religion), nameof(Religion.newReligion)),
         postfix: new HarmonyMethod(GetType(), nameof(set_religion_name)));
-        LogService.LogInfo("语言命名模板加载成功");
+        LogService.LogInfo("宗教Patch加载成功");
     }
 
     private static void set_religion_name(Religion __instance, Actor pActor, WorldTile pTile, bool pAddDefaultTraits)
@@ -35,7 +35,7 @@ public class ReligionPatch : GamePatch
         {
             InsertReligionNameTemplate(__instance, "Western");
         }
-        __instance.SetCity(pActor.city);
+        __instance.SetCity(pActor?.city??pActor?.current_tile?.zone_city);
 
     }
     public static void InsertReligionNameTemplate(Religion religion, string cultureName)
@@ -43,7 +43,7 @@ public class ReligionPatch : GamePatch
         string culturePath = ModPath + $"Cultures/Culture_{cultureName}/";
         string religionNamePath = culturePath + $"{cultureName}ReligionNames.csv";
         List<string> religionKeys = OnomasticsHelper.getKeysFromPath(religionNamePath);
-        religion.data.name = LM.Get(religionKeys[UnityEngine.Random.Range(0, religionKeys.Count)].ToString());
+        religion.data.name = LM.Get(religionKeys[UnityEngine.Random.Range(0, religionKeys.Count)]);
         LogService.LogInfo(cultureName + "宗教名称: " + religion.data.name);
     }
 }
