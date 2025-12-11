db935bc6d3e541c0f03e6982f1890d6a8b66fff6
Zhaoyu Zhang <1304777288@qq.com>
2025-12-11 22:42:06 +1100

    迫使朝贡逻辑修正

--- Scripts/GamePatches/WarPatch.cs
+++ Scripts/GamePatches/WarPatch.cs
@@ -68,17 +68,6 @@ public class WarPatch: GamePatch
             __instance.lostWar(__instance.main_attacker);
             return false;
         }
-        switch (__instance.GetEmpireWarType())
-        {
-            case EmpireWarType.迫使朝贡:
-                if (__instance.main_defender.cities.Count <= 1)
-                {
-                    __instance.lostWar(__instance.main_defender);
-                    __instance.main_defender.JoinTakenAlliance(__instance.main_attacker.GetEmpire());
-                    return false;
-                }
-                break;
-        }
         if (__instance.isTotalWar())
         {
             if (World.world.kingdoms.Count <= 1)
